5cfda70e67cf268d19e49c1b1d37124ab085127c
sunjiahaoz <sunjiahaoz@126.com>
2017-06-27 19:38:13 +0800

    使用DLL了

--- Assets/Demo/Demo.cs
+++ Assets/Demo/Demo.cs
@@ -3,13 +3,15 @@ using System.Collections;
 using KK.Frame.Util;
 
 public class Demo : MonoBehaviour {
-    
+
+    public CameraShake _shake;
 
     void OnGUI()
     {
         if (GUILayout.Button("Click", GUILayout.Width(100), GUILayout.Height(100)))
         {
-            
+            Debug.Log("<color=green>[log]</color>---" + ToolsUseful.GetStackInfo());
+            _shake.DoShake();
         }
     }
 }
---
+++ Assets/Editor/DemoEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using KK.Frame.Util.Editor;
+
+public class DemoEditor : Editor{
+
+    [MenuItem("Tools/SSS")]
+	public static void SSSSS()
+    {
+        ToolsEditor.OpenFolder("C://");
+    }
+}
